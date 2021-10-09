19fa0c4ab3a3af69369be261f7b9e8e474bdf1e8
lomelidev <miguel@lomeli.io>
2021-10-09 12:25:38 -0500

    Agregamos la libreria de variables globales

---
+++ lib/Globals/Globals.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include "Globals.h"
+
+/*Leds*/
+int led = 2;
+
+/*Serial*/
+byte byteCommand = 0x00;
+byte byteAction = 0x00;
+byte byteCommandBLE = 0x00;
+byte byteActionBLE = 0x00;
+int countByte = 0;
+unsigned long LAST_STATUS = millis();
+bool CHECK_STATUS = true;
+bool debug = false;
+
+/*Reset*/
+int pin_reset = 5;
+unsigned long LASTRESET = 0;
+const int timeReset = 5000;
+
+/*BLUETOOTH*/
+std::string nameBluetooth = "ATM Servicios Multimedia";
+uint32_t pinBluetooth = 999999;
+int JUMPER_BLUETOOTH = 15;
---
+++ lib/Globals/Globals.h
@@ -0,0 +1,29 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <Arduino.h>
+
+/*Leds*/
+extern int led;
+
+/*Serial*/
+extern byte byteCommand;
+extern byte byteAction;
+extern byte byteCommandBLE;
+extern byte byteActionBLE;
+extern int countByte;
+extern unsigned long LAST_STATUS;
+extern bool CHECK_STATUS;
+extern bool debug;
+
+/*Reset*/
+extern int pin_reset;
+extern unsigned long LASTRESET;
+extern const int timeReset;
+
+/*BLUETOOTH*/
+extern std::string nameBluetooth;
+extern uint32_t pinBluetooth;
+extern int JUMPER_BLUETOOTH;
+
+#endif
